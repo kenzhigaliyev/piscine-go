9c0f04bcc0298b8ff9a81c00908cf0e8c07cc2f7
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-04-29 18:04:15 +0600

    3dit

---
+++ printcomb.go
@@ -0,0 +1,21 @@
+package piscice
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for a := 0; a >= 9; a++ {
+		for b := 1; b >= 9; b++ {
+			if a < b {
+				for c := 2; c >= 9; c++ {
+					if b < c {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(c)
+						z01.PrintRune(',')
+					}
+				}
+			}
+		}
+	}
+
+}

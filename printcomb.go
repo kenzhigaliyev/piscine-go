c1fb20b83b3fa9eedec1bef8180c356361e33d95
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-04-30 00:18:53 +0600

    newcomb

--- printcomb.go
+++ printcomb.go
@@ -3,10 +3,10 @@ package piscice
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for a := "@"; a >= "I"; a++ {
-		for b := "A"; b >= "I"; b++ {
+	for a := "0"; a >= "9"; a++ {
+		for b := "1"; b >= "9"; b++ {
 			if a < b {
-				for c := "B"; c >= "I"; c++ {
+				for c := "2"; c >= "9"; c++ {
 					if b < c {
 						z01.PrintRune(int(a))
 						z01.PrintRune(int(b))

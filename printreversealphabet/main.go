903a5a6b68fd9e6d91d4361e6c404a2a77b9693b
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-04-30 01:30:04 +0600

    last one

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,8 +3,32 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for i := 'z'; i >= 'a'; i-- {
-		z01.PrintRune(i)
+	// // for i := 'z'; i >= 'a'; i-- {
+	// // 	z01.PrintRune(i)
+	// // }
+	// // z01.PrintRune('\n')
+	// i := 4
+	// z01.PrintRune(int(i))
+	// z01.PrintRune('\n')
+
+	for a := '0'; a <= '9'; a++ {
+		for b := a + 1; b <= '9'; b++ {
+			if a < b {
+				for c := b + 1; c <= '9'; c++ {
+					if b < c {
+						if a == '7' && b == '8' && c == '9' {
+							z01.PrintRune(rune(a))
+							z01.PrintRune(rune(b))
+							z01.PrintRune(rune(c))
+						} else {
+							z01.PrintRune(rune(a))
+							z01.PrintRune(rune(b))
+							z01.PrintRune(rune(c))
+							z01.PrintRune(',')
+						}
+					}
+				}
+			}
+		}
 	}
-	z01.PrintRune('\n')
 }

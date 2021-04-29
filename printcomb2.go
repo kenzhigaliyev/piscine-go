15cc01c9f777adc9f5690ad601797ad423072d53
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-04-30 02:11:04 +0600

    comb2

---
+++ printcomb2.go
@@ -0,0 +1,25 @@
+package piscice
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				for d := '0'; d <= '9'; d++ {
+					e := a + b
+					f := c + d
+					if e < f {
+						z01.PrintRune(rune(a))
+						z01.PrintRune(rune(b))
+						z01.PrintRune(' ')
+						z01.PrintRune(rune(c))
+						z01.PrintRune(rune(d))
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+}

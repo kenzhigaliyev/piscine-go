63bbaad2a3c20a67a176ebfa70a178b7ff4020ed
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-04-30 02:12:16 +0600

    qwertycomb

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -10,22 +10,32 @@ func main() {
 	// i := 4
 	// z01.PrintRune(int(i))
 	// z01.PrintRune('\n')
-
+	// a := '1'
+	// b := '2'
+	// c := '3'
+	// // d := '4'
+	// e := a + b
+	// // f := c + d
+	// if e > c {
+	// 	z01.PrintRune(rune(a))
+	// 	z01.PrintRune('\n')
+	// } else {
+	// 	z01.PrintRune(rune(c))
+	// }
 	for a := '0'; a <= '9'; a++ {
-		for b := a + 1; b <= '9'; b++ {
-			if a < b {
-				for c := b + 1; c <= '9'; c++ {
-					if b < c {
-						if a == '7' && b == '8' && c == '9' {
-							z01.PrintRune(rune(a))
-							z01.PrintRune(rune(b))
-							z01.PrintRune(rune(c))
-						} else {
-							z01.PrintRune(rune(a))
-							z01.PrintRune(rune(b))
-							z01.PrintRune(rune(c))
-							z01.PrintRune(',')
-						}
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				for d := '0'; d <= '9'; d++ {
+					e := a + b
+					f := c + d
+					if e < f {
+						z01.PrintRune(rune(a))
+						z01.PrintRune(rune(b))
+						z01.PrintRune(' ')
+						z01.PrintRune(rune(c))
+						z01.PrintRune(rune(d))
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
 					}
 				}
 			}

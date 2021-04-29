626e4e766a99352c7f3a607ff1d6a3c827839c2d
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-04-30 00:13:32 +0600

    comb

--- printcomb.go
+++ printcomb.go
@@ -3,14 +3,14 @@ package piscice
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for a := 0; a >= 9; a++ {
-		for b := 1; b >= 9; b++ {
+	for a := "@"; a >= "I"; a++ {
+		for b := "A"; b >= "I"; b++ {
 			if a < b {
-				for c := 2; c >= 9; c++ {
+				for c := "B"; c >= "I"; c++ {
 					if b < c {
-						z01.PrintRune(a)
-						z01.PrintRune(b)
-						z01.PrintRune(c)
+						z01.PrintRune(rune(a))
+						z01.PrintRune(rune(b))
+						z01.PrintRune(rune(c))
 						z01.PrintRune(',')
 					}
 				}

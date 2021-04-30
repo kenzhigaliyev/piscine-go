2b34942030d00ebf378cd8eb34fd343279e911d0
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-01 03:31:52 +0600

    2

--- strlen.go
+++ strlen.go
@@ -1,10 +1,24 @@
 package piscine
 
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"piscine"
+// )
+
 func StrLen(s string) int {
-	// a := 0
-	// for _, letter := range s {
-	// 	a++
-	// }
-	a := len(s)
+	a := 0
+	for _, letter := range s {
+		a++
+		_ = letter
+	}
+	// a := len(s)
 	return a
 }
+
+// func main() {
+// 	l := piscine.StrLen("Hello World!")
+// 	fmt.Println(l)
+// }

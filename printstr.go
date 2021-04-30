8cf22d7ec98999369d1f2201165aae0c6a763ddf
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-01 03:09:50 +0600

    17

--- printstr.go
+++ printstr.go
@@ -3,10 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	// st := []s
-	length := len(s)
-	runes := []rune(s)
-	for i:=0;i<=length:i++ {
-		z01.PrintRune(runes)
+	for _, letter := range s {
+		z01.PrintRune(letter)
 	}
 }

1688a13510dad106503e03075bd77c256a7ba2a6
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-01 01:26:12 +0600

    asd

--- ultimatepointone.go
+++ ultimatepointone.go
@@ -1,7 +1,21 @@
-package piscine
+// package piscine
+package main
+
+import (
+	"fmt"
+	// piscine ".."
+)
 
 func UltimatePointOne(n ***int) {
-	*n = *n + 1
-	**n = *n
-	***n = **n
+	***n = ***n + 1
+	// **n = ***n
+	// *n = **n
+}
+
+func main() {
+	a := 0
+	b := &a
+	n := &b
+	piscine.UltimatePointOne(&n)
+	fmt.Println(a)
 }

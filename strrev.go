7b6983de06268a4f97caaba3b04ac393debade4f
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-01 03:54:45 +0600

    revs

--- strrev.go
+++ strrev.go
@@ -2,13 +2,17 @@ package piscine
 
 func StrRev(s string) string {
 	a := 0
+	r := []rune(s)
 	for _, letter := range s {
 		_ = letter
 		a++
 	}
 	for b := 0; b < a/2; b++ {
-		rune[a], rune[a-1-b] = rune[a-1-b], rune[b]
+		x := r[b]
+		y := r[a-b]
+		r[b] = y
+		r[a-b] = x
 	}
-	new := string(rune)
+	new := string(r)
 	return new
 }

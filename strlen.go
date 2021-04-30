ca81d45c50c3e3b20e428ef637bef5d32dad657b
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-01 03:23:13 +0600

    1

--- strlen.go
+++ strlen.go
@@ -1,9 +1,10 @@
 package piscine
 
 func StrLen(s string) int {
-	a := 0
-	for _, letter := range s {
-		a++
-	}
+	// a := 0
+	// for _, letter := range s {
+	// 	a++
+	// }
+	a := len(s)
 	return a
 }

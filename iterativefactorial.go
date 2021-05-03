b1319d46b7e3e889f777317363856d9caf429dea
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-03 18:03:24 +0600

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,17 +5,18 @@ package piscine
 // import "fmt"
 
 func IterativeFactorial(nb int) int {
-	if nb <= 0 {
+	if nb <= 0 || nb < 13 {
 		return 0
+	} else {
+		result := 1
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+		return result
 	}
-	result := 1
-	for i := 1; i <= nb; i++ {
-		result = result * i
-	}
-	return result
 }
 
 // func main() {
-// 	arg := 1
+// 	arg := 1132414124124124
 // 	fmt.Println(IterativeFactorial(arg))
 // }

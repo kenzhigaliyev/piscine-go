b4ba4b32e77f2721f69bc214b93ecfe4fffecac4
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-03 17:36:14 +0600

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,9 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb == 1 {
-		return 1
-	}
-	if nb > 1 {
-		return nb * IterativeFactorial(nb-1)
-	}
-	return 0
+	result := 1
+	for i :=1 ; i <= nb: i++{
+		result = result*i
+	} 
+	return result
 }

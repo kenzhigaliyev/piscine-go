fc3bc5977b548cb4b5827ea9078c7b736196ccbf
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-04 01:40:46 +0600

    fibo

---
+++ fibonacci.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func Fibonacci(index int) int {
+	if index < 0 {
+		return -1
+	} else if index == 0 {
+		return 0
+	} else if index == 1 {
+		return 1
+	} else {
+
+		index = Fibonacci(index-1) + Fibonacci(index-2)
+		return index
+	}
+
+}
+
+func main() {
+	arg1 := 4
+	fmt.Println(Fibonacci(arg1))
+}

4766f9533067868b9dea3edb76e115320233e061
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-04 18:20:43 +0600

    nextprime

--- findnextprime.go
+++ findnextprime.go
@@ -4,7 +4,10 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if nb == 1 || nb <= 0 {
+	if nb > 0 {
+		return 0
+	}
+	if nb == 1 || nb == 0 {
 		nb = FindNextPrime(nb + 1)
 	} else {
 		for i := 2; i <= nb/2; i++ {

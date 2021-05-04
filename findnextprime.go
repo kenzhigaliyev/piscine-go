42237ea4e4d5a73171df4cd6f878576788ed60f5
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-04 18:17:59 +0600

    nextprime

---
+++ findnextprime.go
@@ -0,0 +1,26 @@
+// package main
+
+// import "fmt"
+package piscine
+
+func FindNextPrime(nb int) int {
+	if nb == 1 || nb <= 0 {
+		nb = FindNextPrime(nb + 1)
+	} else {
+		for i := 2; i <= nb/2; i++ {
+			if nb%i == 0 {
+				nb = FindNextPrime(nb + 1)
+			}
+		}
+	}
+	return nb
+}
+
+// func main() {
+// 	fmt.Println(FindNextPrime(5))
+// 	fmt.Println(FindNextPrime(4))
+// 	fmt.Println(FindNextPrime(99))
+// 	fmt.Println(FindNextPrime(42))
+// 	fmt.Println(FindNextPrime(35))
+// 	fmt.Println(FindNextPrime(62))
+// }

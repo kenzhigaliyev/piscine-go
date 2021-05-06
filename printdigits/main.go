ce4cc0dcf2dd7ea1d87e9db7a026ad1594976206
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-06 23:55:55 +0600

    cap

--- printdigits/main.go
+++ printdigits/main.go
@@ -1,10 +1,10 @@
-package main
+// package main
 
-import "github.com/01-edu/z01"
+// import "github.com/01-edu/z01"
 
-func main() {
-	for i := '0'; i <= '9'; i++ {
-		z01.PrintRune(i)
-	}
-	z01.PrintRune('\n')
-}
+// func main() {
+// 	for i := '0'; i <= '9'; i++ {
+// 		z01.PrintRune(i)
+// 	}
+// 	z01.PrintRune('\n')
+// }
--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -1,44 +1,44 @@
-package main
+// package main
 
-import "github.com/01-edu/z01"
+// import "github.com/01-edu/z01"
 
-func main() {
-	// // for i := 'z'; i >= 'a'; i-- {
-	// // 	z01.PrintRune(i)
-	// // }
-	// // z01.PrintRune('\n')
-	// i := 4
-	// z01.PrintRune(int(i))
-	// z01.PrintRune('\n')
-	// a := '1'
-	// b := '2'
-	// c := '3'
-	// // d := '4'
-	// e := a + b
-	// // f := c + d
-	// if e > c {
-	// 	z01.PrintRune(rune(a))
-	// 	z01.PrintRune('\n')
-	// } else {
-	// 	z01.PrintRune(rune(c))
-	// }
-	for a := '0'; a <= '9'; a++ {
-		for b := '0'; b <= '9'; b++ {
-			for c := '0'; c <= '9'; c++ {
-				for d := '0'; d <= '9'; d++ {
-					e := a + b
-					f := c + d
-					if e < f {
-						z01.PrintRune(rune(a))
-						z01.PrintRune(rune(b))
-						z01.PrintRune(' ')
-						z01.PrintRune(rune(c))
-						z01.PrintRune(rune(d))
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
-					}
-				}
-			}
-		}
-	}
-}
+// func main() {
+// // for i := 'z'; i >= 'a'; i-- {
+// // 	z01.PrintRune(i)
+// // }
+// // z01.PrintRune('\n')
+// i := 4
+// z01.PrintRune(int(i))
+// z01.PrintRune('\n')
+// a := '1'
+// b := '2'
+// c := '3'
+// // d := '4'
+// e := a + b
+// // f := c + d
+// if e > c {
+// 	z01.PrintRune(rune(a))
+// 	z01.PrintRune('\n')
+// } else {
+// 	z01.PrintRune(rune(c))
+// }
+// 	for a := '0'; a <= '9'; a++ {
+// 		for b := '0'; b <= '9'; b++ {
+// 			for c := '0'; c <= '9'; c++ {
+// 				for d := '0'; d <= '9'; d++ {
+// 					e := a + b
+// 					f := c + d
+// 					if e < f {
+// 						z01.PrintRune(rune(a))
+// 						z01.PrintRune(rune(b))
+// 						z01.PrintRune(' ')
+// 						z01.PrintRune(rune(c))
+// 						z01.PrintRune(rune(d))
+// 						z01.PrintRune(',')
+// 						z01.PrintRune(' ')
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// }

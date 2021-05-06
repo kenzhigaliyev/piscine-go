d2351cd157d9a819f51a2aca07e04e001a12a6d4
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-07 01:54:17 +0600

    cat

---
+++ revparams/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	counter := 0
+	for index := range os.Args {
+		counter = index
+
+	}
+	for new_index := counter; new_index > 0; new_index-- {
+		for _, new_val := range os.Args[new_index] {
+			z01.PrintRune(new_val)
+		}
+		z01.PrintRune('\n')
+	}
+	// z01.PrintRune('\n')
+}

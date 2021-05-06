994cdf335f6506399466570cdc0a107e130d1cea
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-06 13:28:40 +0600

    index

--- index.go
+++ index.go
@@ -1,7 +1,8 @@
-// package main
-package piscine
+package main
 
-// import "fmt"
+// package piscine
+
+import "fmt"
 
 func Index(s string, toFind string) int {
 	first := []rune(s)
@@ -11,12 +12,16 @@ func Index(s string, toFind string) int {
 	for index1 := range toFind {
 		if (int(letter[index1]) <= 90 && int(letter[index1]) >= 65) || (int(letter[index1]) <= 122 && int(letter[index1]) >= 97) {
 			counter++
+		} else {
+			counter++
 		}
 	}
 
 	counter1 := 0
 	for index2 := range s {
 		if (int(first[index2]) <= 90 && int(first[index2]) >= 65) || (int(first[index2]) <= 122 && int(first[index2]) >= 97) {
+			counter1++
+		} else {
 			counter1++
 		}
 	}
@@ -29,8 +34,10 @@ func Index(s string, toFind string) int {
 	return -1
 }
 
-// func main() {
-// 	fmt.Println(Index("Hello!", "l"))
-// 	fmt.Println(Index("Salut!", "alu"))
-// 	fmt.Println(Index("Ola!", "hOl"))
-// }
+func main() {
+	fmt.Println(Index("Hello!", "l"))
+	fmt.Println(Index("Salut!", "Salut"))
+	fmt.Println(Index("nSSYMr^5v0TQQ", "nSSYMr^5v0TQ"))
+	fmt.Println(Index("Salut!", "alu"))
+	fmt.Println(Index("Ola!", "hOl"))
+}

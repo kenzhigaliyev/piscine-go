f19023d7595d78ed40cd61abb10ce12799efa744
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-06 10:07:02 +0600

    1problem5q

---
+++ firstrune.go
@@ -0,0 +1,17 @@
+// package main
+
+package piscine
+
+// import "github.com/01-edu/z01"
+
+func FirstRune(s string) rune {
+	letter := []rune(s)
+	return letter[0]
+}
+
+// func main() {
+// 	z01.PrintRune(FirstRune("Hello!"))
+// 	z01.PrintRune(FirstRune("Salut!"))
+// 	z01.PrintRune(FirstRune("Ola!"))
+// 	z01.PrintRune('\n')
+// }

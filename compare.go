fae81b4e48de1c83645edf119b9e831714eaffd5
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-06 11:31:03 +0600

    compare

---
+++ compare.go
@@ -0,0 +1,20 @@
+// package main
+
+// import "fmt"
+
+package piscine
+
+func Compare(a, b string) int {
+	if a > b {
+		return 1
+	} else if a < b {
+		return -1
+	}
+	return 0
+}
+
+// func main() {
+// 	fmt.Println(Compare("Hello!", "Hello!"))
+// 	fmt.Println(Compare("Salut!", "lut!"))
+// 	fmt.Println(Compare("Ola!", "Ol"))
+// }

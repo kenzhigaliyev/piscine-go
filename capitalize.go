56e92cb407750be6327fbb560b245e4ab95f5dd5
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-06 23:53:36 +0600

    cap

--- capitalize.go
+++ capitalize.go
@@ -1,61 +1,32 @@
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
-package piscine
+// package piscine
 
 func Capitalize(s string) string {
 	sentence := []rune(s)
-	length := len(sentence)
-	other_chars := true
-	check := true
-	for index := 0; index < length; index++ {
-		if !(sentence[index] <= 122 && sentence[index] >= 97) && !(sentence[index] <= 90 && sentence[index] >= 65) && !(sentence[index] <= 57 && sentence[index] >= 48) {
-			other_chars = true
-		} else if !(sentence[index] <= 122 && sentence[index] >= 97) && other_chars == true {
-			other_chars = false
-			for new_index := index + 1; new_index < length && check; new_index++ {
-				if sentence[new_index] <= 90 && sentence[new_index] >= 65 {
-					sentence[new_index] = sentence[new_index] + 32
-					index++
-				} else if sentence[index] <= 122 && sentence[index] >= 97 {
-					index++
-				} else {
-					other_chars = true
-					check = false
-				}
-			}
-		} else if sentence[index] <= 90 && sentence[index] >= 65 {
-			if !other_chars {
-				sentence[index] = sentence[index] + 32
-			}
-		} else if sentence[index] <= 122 && sentence[index] >= 97 {
-			if other_chars {
+	if sentence[0] >= 97 && sentence[0] <= 122 {
+		sentence[0] = sentence[0] - 32
+	}
+	for index := range s {
+		if sentence[index] >= 97 && sentence[index] <= 122 {
+			sentence[index] = sentence[index]
+		} else if (sentence[index] >= 65 && sentence[index] >= 90) && (sentence[index-1] >= 97 && sentence[index-1] <= 122) && (sentence[index-1] >= 65 && sentence[index-1] >= 90) {
+			sentence[index] = sentence[index]
+		} else if (sentence[index] >= 97 && sentence[index] <= 122) && !(sentence[index-1] >= 97 && sentence[index-1] <= 122) && !(sentence[index-1] >= 65 && sentence[index-1] >= 90) {
+			if !(sentence[index-1] >= 48 && sentence[index-1] <= 57) {
 				sentence[index] = sentence[index] - 32
-				other_chars = false
 			}
+		} else {
 		}
 	}
+
 	return string(sentence)
 }
 
-// for index := range s {
-// 	if sentence[index] <= 122 && sentence[index] >= 97 {
-// 		sentence[index] = sentence[index] - 32
-// 	} else {
-// 	}
-// 	for index1 := range s[index+1:] {
-// 		if sentence[index1] <= 90 && sentence[index1] >= 65 {
-// 			sentence[index1] = sentence[index1] + 32
-// 		} else if sentence[index1] {
-// 			// Capitalize(string(sentence[index:]))
-// 		}
-// 	}
-// }
-// 	return string(sentence)
-// }
-
-// func main() {
-// 	fmt.Println(Capitalize("Hello! How are you? How+are+things+4you?"))
-// 	fmt.Println(Capitalize("abENxNY}F$6g!"))
-// }
+func main() {
+	fmt.Println(Capitalize("Hello! How are you? How+are+things+4you?"))
+	fmt.Println(Capitalize("abENxNY}F$6g!"))
+	fmt.Println(Capitalize("JiNh7HNDza>la"))
+}

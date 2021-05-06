f291f5a6d0b846af9b45e4bc98a017cf7f999349
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-06 13:37:48 +0600

    concat

---
+++ concat.go
@@ -0,0 +1,14 @@
+// package main
+package piscine
+
+// import "fmt"
+
+func Concat(str1 string, str2 string) string {
+	new_string := str1 + str2
+	return new_string
+}
+
+// func main() {
+// 	fmt.Println(Concat("Hello!", " How are you?"))
+
+// }

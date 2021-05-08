d93e0c3ed03d0d12046325df268fbecfe750c71d
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-08 17:39:14 +0600

    app

---
+++ appendrange.go
@@ -0,0 +1,22 @@
+// package main
+package piscine
+
+// import "fmt"
+
+func AppendRange(min, max int) []int {
+	numbers := make([]int, 0)
+
+	if max > min {
+		for i := 0; i < max-min; i++ {
+			numbers = append(numbers, min+i)
+		}
+	} else {
+
+	}
+	return numbers
+}
+
+// func main() {
+// 	fmt.Println(AppendRange(5, 10))
+// 	fmt.Println(AppendRange(10, 5))
+// }

f04e0c04a1000d9e085830f6d694f61b266887af
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-09 00:35:42 +0600

    split

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,8 +1,8 @@
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
-package piscine
+// package piscine
 
 func SplitWhiteSpaces(s string) []string {
 	var words []string
@@ -15,13 +15,17 @@ func SplitWhiteSpaces(s string) []string {
 		} else if word != ' ' {
 			letters = letters + string(word)
 		} else {
-			words = append(words, letters)
-			letters = ""
+			if letters == "" {
+			} else {
+				words = append(words, letters)
+				letters = ""
+			}
 		}
 	}
 	return words
 }
 
-// func main() {
-// 	fmt.Printf("%#v\n", SplitWhiteSpaces("Hello how are you?"))
-// }
+func main() {
+	fmt.Printf("%#v\n", SplitWhiteSpaces("Hello how are you?"))
+	fmt.Printf("%#v\n", SplitWhiteSpaces("H.`G9wbN:}e>  szb,&88gQy]m< Jb?6YpS(Q}2&3 PJ\\/4Ssdi|xnG UH>\\L#N-,IqH{ n^SbU5!6WN^+n k `Sc&#+namj+ na7hvk\\Ti#L$H"))
+}

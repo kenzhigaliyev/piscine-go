73637e92588870b9caf0d6604b5cca19c6145c27
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-10 18:48:18 +0600

    main

--- point/main.go
+++ point/main.go
@@ -8,15 +8,12 @@ type point struct {
 }
 
 func setPoint(ptr *point) {
-
 	ptr.x = 42
 	ptr.y = 21
 }
 
 func main() {
 	points := &point{}
-
 	setPoint(points)
-
 	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }

202e4e7fcc44df255459e7d674feade54413bae8
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-10 23:58:28 +0600

    main

---
+++ displayfile/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	arguments := os.Args[1:2]
+	if len(arguments) == 0 {
+		fmt.Println("File name is missing")
+		return
+	} else if len(arguments) >= 2 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	file, err := ioutil.ReadFile("quest8.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// fmt.Println(file.Stat())
+
+	// arr := make([]byte, 14)
+
+	// file.Read(arr)
+
+	fmt.Print(string(file))
+}

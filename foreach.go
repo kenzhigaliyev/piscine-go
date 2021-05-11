e5c8b47831d0854f1baea3a9c0655e25bc572030
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-12 00:10:20 +0600

    asd

---
+++ foreach.go
@@ -0,0 +1,24 @@
+package piscine
+
+import (
+	"fmt"
+	// "piscine"
+)
+
+func ForEach(f func(int), a []int) {
+	// numbers := make([]int, 6)
+	for i := 0; i < len(a); i++ {
+		PrintNbr(a[i])
+	}
+	fmt.Print("\n")
+}
+
+// func PrintNbr(i int) {
+// 	fmt.Print(i)
+// 	// return i
+// }
+
+// func main() {
+// 	a := []int{1, 2, 3, 4, 5, 6}
+// 	ForEach(PrintNbr, a)
+// }

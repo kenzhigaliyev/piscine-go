430a3d212c421d0c9bd3dc0b5a41cc45474fecfe
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-12 21:27:39 +0600

    sort

--- issorted.go
+++ issorted.go
@@ -2,30 +2,33 @@
 
 package piscine
 
-// "fmt"
+// import "fmt"
 
 // func f(a, b int) int {
-// 	if a > b {
-// 		return 1
-// 	} else if a == b {
-// 		return 0
-// 	}
-// 	return -1
+// 	return a - b
 // }
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	grow := true
+	decr := true
+	// equal := make([]int, len(a))
 	for i := 0; i < len(a)-1; i++ {
 		if f(a[i], a[i+1]) > 0 {
-			return false
-		} else if f(a[i], a[i+1]) == 0 {
-			return false
+			grow = false
 		}
 	}
-	return true
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) < 0 {
+			decr = false
+		}
+	}
+	return grow || decr
 }
 
 // func main() {
 // 	a1 := []int{0, 1, 2, 3, 4, 5}
+// 	// a2 := []int{100, 71, 62, 53, 44, 35}
+
 // 	a2 := []int{0, 2, 1, 3}
 
 // 	result1 := IsSorted(f, a1)

d814eae833c016076c9322e5959d736637c150d2
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-13 20:48:20 +0600

    compact

--- compact.go
+++ compact.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 const N = 6
 
@@ -21,19 +19,19 @@ func Compact(ptr *[]string) int {
 	return len(*ptr)
 }
 
-func main() {
-	a := make([]string, N)
-	a[0] = "a"
-	a[2] = "b"
-	a[4] = "c"
+// func main() {
+// 	a := make([]string, N)
+// 	a[0] = "a"
+// 	a[2] = "b"
+// 	a[4] = "c"
 
-	for _, v := range a {
-		fmt.Println(v)
-	}
+// 	for _, v := range a {
+// 		fmt.Println(v)
+// 	}
 
-	fmt.Println("Size after compacting:", Compact(&a))
-	// fmt.Println(len(a))
-	for _, v := range a {
-		fmt.Println(v)
-	}
-}
+// 	fmt.Println("Size after compacting:", Compact(&a))
+// 	// fmt.Println(len(a))
+// 	for _, v := range a {
+// 		fmt.Println(v)
+// 	}
+// }

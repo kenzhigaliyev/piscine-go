c1a79248939679f27fa427ceeafd8a6b954e86e2
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-13 18:38:41 +0600

    main

---
+++ fixthemain/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type Door struct {
+	state int
+}
+
+const (
+	CLOSE = 0
+	OPEN  = 1
+)
+
+func PrintStr(s string) {
+	// runes := []rune(s)
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func OpenDoor(ptrDoor *Door) {
+	PrintStr("Door Openin...\n")
+	ptrDoor.state = OPEN
+}
+
+func CloseDoor(ptrDoor *Door) bool {
+	PrintStr("Door Closing...\n")
+	return ptrDoor.state == CLOSE
+	// return true
+}
+
+func IsDoorOpen(ptrDoor *Door) bool {
+	PrintStr("is the Door opened ?\n")
+	return ptrDoor.state == OPEN
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("is the Door closed ?\n")
+	return ptrDoor.state == CLOSE
+}
+
+func main() {
+	// CLOSE := 0
+	// OPEN := 0
+	// door := &Door{}
+	var door Door
+	OpenDoor(&door)
+	if IsDoorClose(&door) {
+		OpenDoor(&door)
+	}
+	if IsDoorOpen(&door) {
+		CloseDoor(&door)
+	}
+	if door.state == OPEN {
+		CloseDoor(&door)
+	}
+}

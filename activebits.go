bd07e794e6caf063ba4ac36943643e172263783c
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-13 22:37:31 +0600

    activenits

---
+++ activebits.go
@@ -0,0 +1,21 @@
+// package main
+
+package piscine
+
+// import (
+// 	"fmt"
+// 	// "piscine"
+// )
+
+func ActiveBits(n int) int {
+	binary := 0
+	for n != 0 {
+		n = n / 2
+		binary++
+	}
+	return binary
+}
+
+// func main() {
+// 	fmt.Println(ActiveBits(7))
+// }

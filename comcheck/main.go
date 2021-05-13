9897862af848bc9be3ad71349a02768cd1652c5b
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-13 12:09:27 +0600

    commit

---
+++ comcheck/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	values := os.Args[1:]
+	check := []string{"01", "galaxy", "galaxy 01"}
+	for _, val := range values {
+		for _, str := range check {
+			if val == str {
+				fmt.Println("Alert!!!")
+			}
+		}
+	}
+}

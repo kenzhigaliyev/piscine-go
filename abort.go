e32d4fd7120df1af7c372efa2f47384872a497e2
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-13 09:28:37 +0600

    abort

--- abort.go
+++ abort.go
@@ -1,27 +1,24 @@
 package piscine
 
-import (
-	"sort"
-)
-
-func Abort(a, b, c, d, e int) int {
+func Abort(a, b, c, d, e int) []int {
 	new_array := []int{a, b, c, d, e}
-	sort.Ints(new_array)
-	// var arr int
-	// for i := 0; i < 5; i++ {
-	// 	for j := 0; j < 4; j++ {
-	// 		if new_array[j] > new_array[len(new_array)-i] {
-	// 			arr = new_array[j]
-	// 			new_array[j] = new_array[len(new_array)-i]
-	// 			new_array[len(new_array)-i] = arr
-	// 		}
-	// 	}
-	// }
-	return new_array[2]
+	// sort.Ints(new_array)
+	var arr int
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if new_array[j] > new_array[i] {
+				arr = new_array[j]
+				new_array[j] = new_array[i]
+				new_array[i] = arr
+			}
+		}
+	}
+	return new_array
 }
 
 // func main() {
 // 	middle := Abort(-8301353824951690532, 2626072374859070259, 6993143751468609912, 1580539742955549786, -8821429897402619750)
-
-// 	fmt.Println(middle)
+// 	for i := 0; i < 5; i++ {
+// 		fmt.Println(middle[i])
+// 	}
 // }

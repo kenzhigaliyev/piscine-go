77bf96818d24d73448cbbd59272dad0e13caab85
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-13 14:09:22 +0600

    enigma

---
+++ enigma.go
@@ -0,0 +1,49 @@
+package main
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	new := 0
+	new = *******c
+	*******c = ***a
+	***a = *b
+	*b = ****d
+	****d = new
+}
+
+// func main() {
+// 	x := 5
+// 	y := &x
+// 	z := &y
+// 	a := &z
+
+// 	w := 2
+// 	b := &w
+
+// 	u := 7
+// 	e := &u
+// 	f := &e
+// 	g := &f
+// 	h := &g
+// 	i := &h
+// 	j := &i
+// 	c := &j
+
+// 	k := 6
+// 	l := &k
+// 	m := &l
+// 	n := &m
+// 	d := &n
+
+// 	fmt.Println(***a)
+// 	fmt.Println(*b)
+// 	fmt.Println(*******c)
+// 	fmt.Println(****d)
+
+// 	Enigma(a, b, c, d)
+
+// 	fmt.Println("After using Enigma")
+// 	fmt.Println(***a)
+// 	fmt.Println(*b)
+// 	fmt.Println(*******c)
+// 	fmt.Println(****d)
+
+// }

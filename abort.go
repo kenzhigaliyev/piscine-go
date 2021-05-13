595806700a5615c016577184a14502fe8f049580
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-13 08:36:29 +0600

    abort

---
+++ abort.go
@@ -0,0 +1,11 @@
+package piscine
+
+func Abort(a, b, c, d, e int) int {
+	median := (a + b + c + d + e) / 5
+	return median
+}
+
+// func main() {
+// 	middle := Abort(2, 3, 8, 5, 7)
+// 	fmt.Println(middle)
+// }

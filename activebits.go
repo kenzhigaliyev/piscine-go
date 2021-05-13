a7ba57f70b8318a140b424729358a7fa5e9144b4
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-13 22:42:54 +0600

    active

--- activebits.go
+++ activebits.go
@@ -1,21 +1,30 @@
-package main
+// package main
 
-// package piscine
+package piscine
 
-import (
-	"fmt"
-	// "piscine"
-)
+// import (
+// 	"fmt"
+// 	// "piscine"
+// )
 
 func ActiveBits(n int) int {
 	binary := 0
+	if n%2 == 0 {
+		for n != 1 {
+			n = n / 2
+			binary++
+		}
+		return binary
+	}
 	for n != 0 {
 		n = n / 2
 		binary++
 	}
 	return binary
+
 }
 
-func main() {
-	fmt.Println(ActiveBits(14))
-}
+// func main() {
+// 	fmt.Println(ActiveBits(14))
+// 	fmt.Println(ActiveBits(7))
+// }

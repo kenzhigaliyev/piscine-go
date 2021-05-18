baa482ae187a96d3ef3cc98b2479d02e9c007f73
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-18 19:54:08 +0600

    listfromnt

---
+++ listpushfront.go
@@ -0,0 +1,38 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+
+	val := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = val
+		return
+	}
+	val.Next = l.Head
+	l.Head = val
+}
+
+// func main() {
+
+// 	link := &List{}
+
+// 	ListPushFront(link, "Hello")
+// 	ListPushFront(link, "man")
+// 	ListPushFront(link, "how are you")
+
+// 	it := link.Head
+// 	for it != nil {
+// 		fmt.Print(it.Data, " ")
+// 		it = it.Next
+// 	}
+// 	fmt.Println()
+// }

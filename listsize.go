413d5ab5c4f0dc4475f892a84f9334c2f24b6065
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-18 20:36:50 +0600

    listsize

---
+++ listsize.go
@@ -0,0 +1,49 @@
+// package main
+
+package piscine
+
+// import (
+// 	"fmt"
+// )
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListSize(l *List) int {
+	point := l.Head
+	counter := 0
+	for point != nil {
+		counter++
+		point = point.Next
+	}
+	return counter
+}
+
+func ListPushFront(l *List, data interface{}) {
+	val := &NodeL{Data: data}
+	if l.Head == nil {
+		l.Head = val
+		return
+	}
+	val.Next = l.Head
+	l.Head = val
+
+}
+
+// func main() {
+// 	link := &List{}
+
+// 	ListPushFront(link, "Hello")
+// 	ListPushFront(link, "2")
+// 	ListPushFront(link, "you")
+// 	ListPushFront(link, "man")
+
+// 	fmt.Println(ListSize(link))
+// }

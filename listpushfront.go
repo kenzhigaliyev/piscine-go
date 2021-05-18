07f353493eada1e007a622cccc1c2f01f53f6f0a
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-18 20:10:32 +0600

    last

--- listpushfront.go
+++ listpushfront.go
@@ -1,14 +1,14 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
 
 func ListPushFront(l *List, data interface{}) {
 	val := &NodeL{Data: data}

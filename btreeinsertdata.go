af2f1f060d3c27d4a00bd7fb9e8c5af0f6edb151
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-20 11:06:02 +0600

    bt

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -15,11 +15,9 @@ func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 	if root == nil {
 		return &TreeNode{Data: data}
 	}
-
 	if data < root.Data {
 		root.Left = BTreeInsertData(root.Left, data)
 	} else {
-
 		root.Right = BTreeInsertData(root.Right, data)
 	}
 	return root

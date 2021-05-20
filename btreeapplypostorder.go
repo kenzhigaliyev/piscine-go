84e16d11d4e80e95e96c8839b306b6195fc089ca
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-20 14:43:03 +0600

    new

--- btreeapplypostorder.go
+++ btreeapplypostorder.go
@@ -11,17 +11,17 @@ package piscine
 // 	Data                string
 // }
 
-// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-// 	if root == nil {
-// 		return &TreeNode{Data: data}
-// 	}
-// 	if data < root.Data {
-// 		root.Left = BTreeInsertData(root.Left, data)
-// 	} else {
-// 		root.Right = BTreeInsertData(root.Right, data)
-// 	}
-// 	return root
-// }
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+	}
+	return root
+}
 
 func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root.Left != nil {

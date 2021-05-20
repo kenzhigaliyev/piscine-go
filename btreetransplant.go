0a573dc2eeebc27f34547b73fe5c63487eac8c12
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-21 01:13:47 +0600

    trans

--- btreetransplant.go
+++ btreetransplant.go
@@ -43,18 +43,15 @@ package piscine
 // }
 
 func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
-	if root == nil {
-		return nil
+	if node.Parent.Left == node {
+		node.Parent.Left = rplc
+		rplc.Parent = node.Parent
+		node = nil
+	} else if node.Parent.Right == node {
+		node.Parent.Right = rplc
+		rplc.Parent = node.Parent
+		node = nil
 	}
-	replacement := node
-	if node.Parent == nil {
-		root = rplc
-	} else if node == node.Parent.Left {
-		replacement.Parent.Left = rplc
-	} else {
-		replacement.Parent.Right = rplc
-	}
-	replacement.Parent = node.Parent
 	return root
 }
 

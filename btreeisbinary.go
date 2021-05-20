337570edd4a9596eb071b72029a21ee651d60e38
kenzhigaliyev <adil.kenzhigaliyev@nu.edu.kz>
2021-05-20 19:38:25 +0600

    isbinary

---
+++ btreeisbinary.go
@@ -0,0 +1,49 @@
+// package main
+
+package piscine
+
+// import (
+// 	"fmt"
+// )
+
+// type TreeNode struct {
+// 	Left, Right, Parent *TreeNode
+// 	Data                string
+// }
+
+// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+// 	if root == nil {
+// 		return &TreeNode{Data: data}
+// 	}
+// 	if data < root.Data {
+// 		root.Left = BTreeInsertData(root.Left, data)
+// 	} else {
+// 		root.Right = BTreeInsertData(root.Right, data)
+// 	}
+// 	return root
+// }
+
+func BTreeIsBinary(root *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left != nil {
+		if root.Left.Data > root.Data {
+			return false
+		}
+	}
+	if root.Right != nil {
+		if root.Right.Data < root.Data {
+			return false
+		}
+	}
+	return (BTreeIsBinary(root.Left) && BTreeIsBinary(root.Right))
+}
+
+// func main() {
+// 	root := &TreeNode{Data: "4"}
+// 	BTreeInsertData(root, "1")
+// 	BTreeInsertData(root, "7")
+// 	BTreeInsertData(root, "5")
+// 	fmt.Println(BTreeIsBinary(root))
+// }
